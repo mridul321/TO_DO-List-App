a7d9ac002355d142514abc885e913ae475ea5968
Mridul <mridulpandey321@gmail.com>
2019-08-14 18:53:58 +0530

    Listing App!

---
+++ lib/screens/add_task_screen.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todoey_flutter/modal/task_data.dart';
+
+String newTaskTitle;
+
+class AddTaskScreen extends StatelessWidget {
+
+
+
+  @override
+  Widget build(BuildContext context) {
+
+
+
+    return Container(
+      color: Color(0xff757575),
+      child: Container(
+        padding: EdgeInsets.all(20.0),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20.0),
+            topRight: Radius.circular(20.0),
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Text('Add Task',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 30.0,
+              color: Colors.lightBlueAccent,
+             ),
+            ),
+            TextField(
+              autofocus: true,
+              textAlign: TextAlign.center,
+              onChanged: (newText){
+                newTaskTitle = newText;
+              },
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            FlatButton(
+              child: Text('Add',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20.0,
+               ),
+              ),
+              color: Colors.lightBlueAccent,
+              onPressed: (){
+                Provider.of<TaskData>(context).addTask(newTaskTitle);
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
